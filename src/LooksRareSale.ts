ff55730ce9c6f520fa47a4deacdd46d55a38eb34
Data Nexus <derek.m.meyer@gmail.com>
2022-03-24 23:46:57 -0400

    Create LooksRareSale.ts

---
+++ src/LooksRareSale.ts
@@ -0,0 +1,99 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  LooksRare,
+  CancelAllOrders,
+  CancelMultipleOrders,
+  NewCurrencyManager,
+  NewExecutionManager,
+  NewProtocolFeeRecipient,
+  NewRoyaltyFeeManager,
+  NewTransferSelectorNFT,
+  OwnershipTransferred,
+  RoyaltyPayment,
+  TakerAsk,
+  TakerBid,
+  TakerAsk__Params
+} from "../generated/LooksRare/LooksRare"
+import { collection } from "../generated/schema"
+import { token } from "../generated/schema"
+import { transfer } from "../generated/schema"
+
+
+//export function handleCancelAllOrders(event: CancelAllOrders): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+//  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+//  if (!entity) {
+//    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+//    entity.count = BigInt.fromI32(0)
+//  }
+
+  // BigInt and BigDecimal math are supported
+//  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+//  entity.user = event.params.user
+//  entity.newMinNonce = event.params.newMinNonce
+
+  // Entities can be written to the store with `.save()`
+//  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.DOMAIN_SEPARATOR(...)
+  // - contract.WETH(...)
+  // - contract.currencyManager(...)
+  // - contract.executionManager(...)
+  // - contract.isUserOrderNonceExecutedOrCancelled(...)
+  // - contract.owner(...)
+  // - contract.protocolFeeRecipient(...)
+  // - contract.royaltyFeeManager(...)
+  // - contract.transferSelectorNFT(...)
+  // - contract.userMinOrderNonce(...)
+//}
+
+export function handleTakerAsk(event: TakerAsk): void {
+
+  let transferEntity = transfer.load(event.transaction.hash.toHex())
+
+  if (!transferEntity) {
+    transferEntity = new transfer(event.transaction.from.toHex())
+    
+    transferEntity.id = event.transaction.hash.toHex()
+    //transferEntity.collectionId = 
+    //transferEntity.tokenId = 'test'
+    transferEntity.blockNum = event.block.number.toI32()
+    transferEntity.senderAddress = event.transaction.from
+    transferEntity.receiverAddress = event.transaction.to   //event.transaction.to is the LR contract address not the token recipient
+    transferEntity.amount = event.transaction.value.toBigDecimal()
+    transferEntity.platform = 'LooksRare'
+
+  }
+  
+  transferEntity.save()
+
+}
+
+export function handleTakerBid(event: TakerBid): void {
+
+  let entity = transfer.load(event.transaction.hash.toHex())
+
+}
